79d4b7c8c232a0ca3dcc1f6a4a2e01e893d324f6
Rodrigo <roy_casta@hotmail.com>
2025-08-24 10:43:19 +0200

    RODRIGO CASTAÑEDA COMMIT DEL RETURN

--- primos.c
+++ primos.c
@@ -1,17 +1,11 @@
-// Fichero que retorna 1 o 0 en funcion de si el numero
-// que recibe como parametro de entrada es primo o no.
 #include "lpc17xx.h"
 uint8_t esprimo(uint16_t primo){
 uint16_t divisor;
-uint16_t divisores=0;
+
 	
-	for (divisor=2;divisor<primo;divisor++){
-		if((primo%divisor)==0){
-			divisores++;
-		}
-}
-if(divisores == 0)
+	for (divisor=2;divisor<primo;divisor++)
+		if((primo%divisor)==0)
+			return 0;
 	return 1;
-else
-	return 0;
+
 }
