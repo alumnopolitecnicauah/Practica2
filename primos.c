86e6a29e6a714236649c2e2c655b181f2b737641
Rodrigo <roy_casta@hotmail.com>
2025-08-24 10:35:36 +0200

    RODRIGO CASTAÑEDA COMMIT DE RAMA DEVEL SQRT

--- primos.c
+++ primos.c
@@ -1,11 +1,10 @@
-// Fichero que retorna 1 o 0 en funcion de si el numero
-// que recibe como parametro de entrada es primo o no.
 #include "lpc17xx.h"
+#include <math.h>
 uint8_t esprimo(uint16_t primo){
 uint16_t divisor;
 uint16_t divisores=0;
 	
-	for (divisor=2;divisor<primo;divisor++){
+	for(divisor=sqrt(primo+1);divisor>=2;divisor--){
 		if((primo%divisor)==0){
 			divisores++;
 		}
